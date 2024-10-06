da163bc41e8ed2c537cfc085323eaf4c8fb7869e
Hassan Javaid <hassanjrao@gmail.com>
2024-10-06 16:08:52 +0400

    added profile

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::firstOrCreate([
+        User::updateOrCreate([
             'email' => 'admin@m.com'
         ],[
             'name' => 'Admin',
